a7fd65b0a35b01217cedfe0169b5050ed8b491db
hhl <3334972508@qq.com>
2023-04-27 22:49:50 +0800

    changefirst

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,13 @@
     <router-view/>
   </div>
 </template>
+<script lang='ts'>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App'
+
+})
+</script>
 
 <style lang="scss" scoped></style>
